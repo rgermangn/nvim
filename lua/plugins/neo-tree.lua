7de6326b53dcd63f59c99d301b6f72d470cda675
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-03-30 13:11:06 -0300

    adiciona a configuração de largura da neo-tree
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      window = {
+        width = 25, -- Define a largura da janela do Neo-Tree
+      },
+    })
+
+    -- Mapeia a tecla para abrir/fechar o Neo-Tree
     vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
   end,
 }
