076e57848087d30cc01901736a44622dcb6f2b8a
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-03-30 00:28:04 -0300

    adiciona o ruff
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,67 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ruff",
+          "lua_ls",
+          "ts_ls",
+          "dockerls",
+          "docker_compose_language_service"
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ruff.setup({
+        init_options = {
+          settings = {
+            -- Configurações do Ruff
+            lint = {
+              run = "onSave", -- ou "onType" para linting enquanto digita
+            },
+            -- Aqui estão as opções equivalentes ao pyproject.toml
+            lineLength = 88,
+            indentWidth = 4,
+            targetVersion = "py39",
+            select = {
+              "E",    -- pycodestyle errors
+              "F",    -- pyflakes
+              "I",    -- isort
+              "W",    -- pycodestyle warnings
+              "B",    -- flake8-bugbear
+              "C4",   -- flake8-comprehensions
+            },
+            ignore = {
+              "E501",  -- line too long
+            },
+            format = {
+              quoteStyle = "double",
+              indentStyle = "space",
+              lineEnding = "auto",
+            },
+          }
+        }
+      })
+      lspconfig.ts_ls.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.docker_compose_language_service.setup({})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,22 +4,8 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"html",
-				"css",
-				"python",
-				"dockerfile",
-				"gitignore",
-				"json",
-				"php",
-			},
-			sync_install = false,
-			highlight = { enable = true },
+			auto_install = true,
+      highlight = { enable = true },
 			indent = { enable = true },
 		})
 	end,
