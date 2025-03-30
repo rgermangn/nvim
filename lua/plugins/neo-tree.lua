e379a4ab9b4346f6701a5a388ec8365b7591b090
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-03-29 21:30:53 -0300

    adiciona o none-ls para lua com stylua
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,13 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    -- Mapeia Neo-tree
-    vim.keymap.set('n', '<C-n>',':Neotree toggle left<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		-- Mapeia Neo-tree
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "javascript",
-        "html",
-        "css",
-        "python",
-        "dockerfile",
-        "gitignore",
-        "json",
-        "php"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },  
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"javascript",
+				"html",
+				"css",
+				"python",
+				"dockerfile",
+				"gitignore",
+				"json",
+				"php",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
