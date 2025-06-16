53332ddcf0d037ab9f967cae5288ae7f4689fb40
Roberto German Guedes Neto <robertoguedes@edu.univali.br>
2025-06-16 15:21:58 -0300

    nova arte alpha-nvim e atualizações
    Signed-off-by: Roberto German Guedes Neto <robertoguedes@edu.univali.br>

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -40,8 +40,8 @@ return {
 			enable_diagnostics = true,
 			-- enable_normal_mode_for_inputs = false,                             -- Enable normal mode for input dialogs.
 			open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-			sort_case_insensitive = false, -- used when sorting files and directories in the tree
-			sort_function = nil, -- use a custom function for sorting files and directories in the tree
+			sort_case_insensitive = false,                            -- used when sorting files and directories in the tree
+			sort_function = nil,                                      -- use a custom function for sorting files and directories in the tree
 			-- sort_function = function (a,b)
 			--       if a.type == b.type then
 			--           return a.path > b.path
@@ -127,7 +127,7 @@ return {
 			commands = {},
 			window = {
 				position = "left",
-				width = 40,
+				width = 25,
 				mapping_options = {
 					noremap = true,
 					nowait = true,
@@ -224,11 +224,11 @@ return {
 					},
 				},
 				follow_current_file = {
-					enabled = false, -- This will find and focus the file in the active buffer every time
+					enabled = false,        -- This will find and focus the file in the active buffer every time
 					--               -- the current file is changed while the tree is open.
 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 				},
-				group_empty_dirs = false, -- when true, empty folders will be grouped together
+				group_empty_dirs = false,   -- when true, empty folders will be grouped together
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
 				-- "open_current",  -- netrw disabled, opening a directory opens within the
